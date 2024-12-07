161a36a8df8b5cca045e9bb3ac58d2068e2eb65a
dtheran <dtheran1.0@gmail.com>
2024-12-07 15:05:48 -0500

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from "./components/toolbar/toolbar.component";
 import { WelcomeComponent } from "./components/welcome/welcome.component";
+import { SocialMediaComponent } from "./components/socialMedia/social-media.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent, WelcomeComponent],
+  imports: [RouterOutlet, ToolbarComponent, WelcomeComponent, SocialMediaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
